3da3eab505d8de6bbc6a27d5b0859212b928025f
Smartleasenl <marketing@smartlease.nl>
2026-03-14 01:40:29 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import HomePage from './components/HomePage';
-import Contact from './components/Contact';
+import ServicesPage from './components/ServicesPage';
+import AboutPage from './components/AboutPage';
+import PortfolioPage from './components/PortfolioPage';
+import ContactPage from './components/ContactPage';
 import Footer from './components/Footer';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
   return (
     <div className="min-h-screen">
       <Navigation />
-      <HomePage />
-      <Contact />
-      <Footer />
+      <Routes>
+        <Route path="/"          element={<><HomePage /><Footer /></>} />
+        <Route path="/services"  element={<><ServicesPage /><Footer /></>} />
+        <Route path="/about"     element={<><AboutPage /><Footer /></>} />
+        <Route path="/portfolio" element={<><PortfolioPage /><Footer /></>} />
+        <Route path="/contact"   element={<><ContactPage /><Footer /></>} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
